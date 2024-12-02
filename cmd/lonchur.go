fa774420af657ee4058126666e13f8188a61df38
Andrew G. Neumann <agneum14@gmail.com>
2024-12-02 09:13:16 -0500

    Implement a basic top bar

--- cmd/lonchur.go
+++ cmd/lonchur.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/a-h/templ"
+	"github.com/agneum14/lonchur/views/components"
 	"github.com/agneum14/lonchur/views/pages"
 	"github.com/labstack/echo/v4"
 )
@@ -15,10 +16,17 @@ func render(c echo.Context, cmp templ.Component) error {
 }
 
 func main() {
+	colors := components.Colors{
+		Title: "#BD93F9",
+		Background: "#282A36",
+		Text: "#F8F8F2",
+		BackgroundLight: "#44475A",
+	}
+
 	e := echo.New()
-    e.Static("/static", "assets")
+	e.Static("/static", "assets")
 	e.GET("/", func(c echo.Context) error {
-		return render(c, pages.Home())
+		return render(c, pages.Home(colors))
 	})
 	e.Logger.Fatal(e.Start(":3000"))
 }
